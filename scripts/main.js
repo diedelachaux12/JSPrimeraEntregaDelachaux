99b42bfe673e9593886ffea19aae8df8fbdd11c7
diedelachaux <delachauxdiego@gmail.com>
2023-11-01 20:39:03 -0300

    Se actualiza estilo de boton eliminar

--- scripts/main.js
+++ scripts/main.js
@@ -4,84 +4,90 @@ document.addEventListener("DOMContentLoaded", function () {
   const carritoTotal = document.getElementById("carrito-total");
   const vaciarCarritoBtn = document.getElementById("vaciar-carrito");
 
-     // Inicializamos el carrito desde el Local Storage
-     let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  // Inicializamos el carrito desde el Local Storage 
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-  // Objeto con precios de los productos
+  // Definir un objeto con los precios de los productos
   const preciosProductos = {
-    "Jordan 1 Low": 100,
-    "Air Force 1 '07 Premium": 120,
-    "Air Jordan 1 Mid SE": 110,
-    "Dunk Low Retro 'Panda'": 90
-
+      "Jordan 1 Low": 100,
+      "Air Force 1 '07 Premium": 120,
+      "Air Jordan 1 Mid SE": 110,
+      "Dunk Low Retro 'Panda'": 90
+      
   };
 
-  // Función para actualizar la vista del carrito
+  // Función para actualizar el carrito y guardar en el Local Storage
   function actualizarCarrito() {
-    // Limpiamos la vista del carrito
-    carritoItems.innerHTML = "";
 
-    // Calculamos el total
-    let total = 0;
+      // Limpiamos la vista del carrito
+      carritoItems.innerHTML = "";
 
-    carrito.forEach(item => {
-      const li = document.createElement("li");
-      li.textContent = `${item.nombre} - ${item.precio} USD`;
+      // Calculamos el total
+      let total = 0;
 
-      const eliminarBtn = document.createElement("button");
-      eliminarBtn.textContent = "Eliminar";
-      eliminarBtn.classList.add("boton-eliminar");
-      eliminarBtn.addEventListener("click", () => {
-        eliminarItemDelCarrito(item);
+      carrito.forEach(item => {
+          const li = document.createElement("li");
+          li.textContent = `${item.nombre} - ${item.precio} USD`;
+
+          const eliminarBtn = document.createElement("button");
+          eliminarBtn.textContent = "Eliminar";
+          eliminarBtn.addEventListener("click", () => {
+              eliminarItemDelCarrito(item);
+          });
+
+          eliminarBtn.style.backgroundColor = "#c62d1f";
+          eliminarBtn.style.color = "white";
+          eliminarBtn.style.border = "none";
+          eliminarBtn.style.padding = "5px 10px";
+          eliminarBtn.style.borderRadius = "4px";
+          eliminarBtn.style.cursor = "pointer";
+          eliminarBtn.style.fontSize = "14px";
+
+          li.appendChild(eliminarBtn);
+          carritoItems.appendChild(li);
+
+          total += item.precio;
       });
 
-      eliminarBtn.style.backgroundColor = "#c62d1f";
-      eliminarBtn.style.color = "white";
-      eliminarBtn.style.border = "none";
-      eliminarBtn.style.padding = "5px 10px";
-      eliminarBtn.style.borderRadius = "4px";
-      eliminarBtn.style.cursor = "pointer";
-      eliminarBtn.style.fontSize = "14px";
+      carritoTotal.textContent = total;
 
-      li.appendChild(eliminarBtn);
-      carritoItems.appendChild(li);
-
-      total += item.precio;
-    });
-
-    carritoTotal.textContent = total;
+      // Guardar el carrito en el Local Storage
+      localStorage.setItem("carrito", JSON.stringify(carrito));
   }
 
   // Función para agregar un item al carrito
   function agregarAlCarrito(nombre, precio) {
-    carrito.push({ nombre, precio });
-    actualizarCarrito();
+      carrito.push({ nombre, precio });
+      actualizarCarrito();
   }
 
   // Función para eliminar un item del carrito
   function eliminarItemDelCarrito(item) {
-    const index = carrito.indexOf(item);
-    if (index !== -1) {
-      carrito.splice(index, 1);
-      actualizarCarrito();
-    }
+      const index = carrito.indexOf(item);
+      if (index !== -1) {
+          carrito.splice(index, 1);
+          actualizarCarrito();
+      }
+      
   }
 
   // Manejadores de eventos para los botones "Comprar"
   const botonesComprar = document.querySelectorAll(".boton-comprar");
   botonesComprar.forEach(boton => {
-    boton.addEventListener("click", () => {
-      const tarjeta = boton.closest(".tarjeta");
-      const nombre = tarjeta.querySelector(".titulo-tarjeta").textContent;
-      const precio = preciosProductos[nombre]; // Obtener el precio del objeto
-      agregarAlCarrito(nombre, precio);
-    });
+      boton.addEventListener("click", () => {
+          const tarjeta = boton.closest(".tarjeta");
+          const nombre = tarjeta.querySelector(".titulo-tarjeta").textContent;
+          const precio = preciosProductos[nombre]; // Obtener el precio del objeto
+          agregarAlCarrito(nombre, precio);
+      });
   });
 
   // Manejador de evento para el botón "Vaciar Carrito"
   vaciarCarritoBtn.addEventListener("click", () => {
-    carrito = [];
-    actualizarCarrito();
+      carrito = [];
+      actualizarCarrito();
+
+      // Eliminar el carrito del Local Storage
+      localStorage.removeItem("carrito");
   });
 });
-
